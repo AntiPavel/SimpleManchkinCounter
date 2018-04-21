ae39defd17c792aea94a884c4164c0f891a4936d
pavel.antonov <pavel.antonov@accenture.com>
2018-04-21 18:44:29 +0300

    players and player manager updates

--- SimpleManchkinCounter/SimpleManchkinCounter/Models/Manchkin.swift
+++ SimpleManchkinCounter/SimpleManchkinCounter/Models/Manchkin.swift
@@ -14,50 +14,34 @@ final class Manchkin: Player {
         self.id = id
     }
     
-    func setName(name: String) {
-        storedName = name
+    init?(dict: [String : Any]) {
+        guard   let storedName = dict["name"] as? String,
+                let id = dict["id"] as? Int else {
+            return nil
+        }
+        self.storedName = storedName
+        self.id = id
     }
     
-    func getName() -> String {
-        return storedName
+    func clone() -> Player {
+        let clone = Manchkin(id: id)
+        clone.bonus = bonus
+        clone.level = level
+        clone.storedName = "clone \(storedName)"
+        clone.isCursedNow = isCursedNow
+        return clone
     }
     
-    func increaseLevel() {
-        level += 1
+    var id: Int
+    var level: Int = 1 {
+        didSet {
+            if level < 1 { level = 1 }
+        }
     }
-    
-    func decreaseLevel() {
-        level -= 1
+    var streight: Int {
+        get { return level + bonus }
     }
-    
-    func increaseBonus() {
-        bonus += 1
-    }
-    
-    func decreaseBonus() {
-        bonus -= 1
-    }
-    
-    func setCourse(coursed: Bool) {
-        isCursedNow = coursed
-    }
-    
-    func getStreight() -> Int {
-        return streight + level
-    }
-    
-    func isCursed() -> Bool {
-        return isCursedNow
-    }
-    
-    func getId() -> Int {
-        return id
-    }
-    
-    private let id: Int
-    private var streight: Int = 0
-    private var level: Int = 0
-    private var bonus: Int = 0
-    private var isCursedNow: Bool = false
-    private var storedName: String = "Player"
+    var bonus: Int = 0
+    var isCursedNow: Bool = false
+    var storedName: String = "Player"
 }
--- SimpleManchkinCounter/SimpleManchkinCounter/PlayerManager/PlayerManager.swift
+++ SimpleManchkinCounter/SimpleManchkinCounter/PlayerManager/PlayerManager.swift
@@ -8,47 +8,71 @@
 
 import Foundation
 
+let playerIdBaseKey = "playerIdBase"
+let storedPlayers = "storedPlayers"
+
 protocol PlayerManager {
     
-    func addManchkin()
+    func addNewManchkin()
+    func addManchkin(name: String)
     func addMonster()
+    func clonePlayer(id: Int) -> Player?
     func removePlayer(id: Int)
     func getMonsters() -> Array<Monster>
     func getManchkins() -> Array<Manchkin>
     func getMonster(id: Int) -> Monster?
     func getManchkin(id: Int) -> Manchkin?
+    func setName(name: String, id: Int) -> Bool
+    func getName(id: Int) -> String?
+    func increasePlayerLevel(_ n: Int, id: Int) -> Bool
+    func decreasePlayerLevel(_ n: Int, id: Int) -> Bool
+    func increaseManchkinBonus(_ n: Int, id: Int) -> Bool
+    func decreaseManchkinBonus(_ n: Int, id: Int) -> Bool
+    func changeCourseState(id: Int) -> Bool
+    func getStreight(id: Int) -> Int?
+    func isCursed(id: Int) -> Bool
+    func getStoredManchkins() -> Array<Manchkin>
+    func removeStoredManchkin(name: String)
+
 }
 
 final class SimplePlayerManager: PlayerManager {
     
     static let shared: SimplePlayerManager = {
         let instance = SimplePlayerManager()
-        if let id = UserDefaults.standard.object(forKey: playerIdKey) as? Int, id != 0 {
-            // the id already exist so nothing to do
-        } else {
-            let uuid = UUID().hashValue
-            UserDefaults.standard.set(uuid, forKey: playerIdKey)
-        }
+        instance.setup()
         return instance
     }()
     
-    func addManchkin() {
-        let id: Int = UserDefaults.standard.object(forKey: SimplePlayerManager.getPlayerIdKey()) as? Int ?? Int(arc4random_uniform(99999999))
-        UserDefaults.standard.set(id + 1, forKey: SimplePlayerManager.getPlayerIdKey())
-        let manchkin = Manchkin(id: id)
-        manchkins.append(manchkin)
+    private func setup() {
+        createAndStoreIdBaseIfNeed()
+        storedManchkins = getStoredManchkins()
+    }
+    
+    func addNewManchkin() {
+        manchkins.append(playerFabric.getManchkin())
+    }
+    
+    func addManchkin(name: String) {
+        let newManchkin = playerFabric.getManchkin()
+        manchkins.append(newManchkin)
     }
     
     func addMonster() {
-        let id: Int = UserDefaults.standard.object(forKey: SimplePlayerManager.getPlayerIdKey()) as? Int ?? Int(arc4random_uniform(99999999))
-        UserDefaults.standard.set(id + 1, forKey: SimplePlayerManager.getPlayerIdKey())
-        let monster = Monster(id: id)
-        monsters.append(monster)
+        monsters.append(playerFabric.getMonster())
+    }
+    
+    func clonePlayer(id: Int) -> Player? {
+        guard let player = getPlayer(id: id) else { return nil }
+        var clone = player.clone()
+        let newPlayerWithNewId = playerFabric.getManchkin()
+        clone.id = newPlayerWithNewId.id
+        return clone
     }
     
     func removePlayer(id: Int) {
-        manchkins = manchkins.filter{ $0.getId() != id }
-        monsters = monsters.filter{ $0.getId() != id }
+        manchkins = manchkins.filter{ $0.id != id }
+        monsters = monsters.filter{ $0.id != id }
     }
     
     func getMonsters() -> Array<Monster> {
@@ -60,21 +84,132 @@ final class SimplePlayerManager: PlayerManager {
     }
     
     func getMonster(id: Int) -> Monster? {
-        let monster = monsters.filter{ $0.getId() == id }
+        let monster = monsters.filter{ $0.id == id }
         return monster.first
     }
     
     func getManchkin(id: Int) -> Manchkin? {
-        let manchkin = manchkins.filter{ $0.getId() == id }
+        let manchkin = manchkins.filter{ $0.id == id }
         return manchkin.first
     }
     
-    static private func getPlayerIdKey() -> String {
-        return playerIdKey
+    func setName(name: String, id: Int) -> Bool {
+        guard let manchkin = getManchkin(id: id) else { return false }
+        manchkin.storedName = name
+        removeStoredManchkin(name: manchkin.storedName)
+        storedManchkins.append(manchkin)
+        return true
+    }
+    
+    func getName(id: Int) -> String? {
+        guard let manchkin = getManchkin(id: id) else { return nil }
+        return manchkin.storedName
+    }
+    
+    func increasePlayerLevel(_ n: Int, id: Int) -> Bool  {
+        guard var player = getPlayer(id: id) else { return false }
+        player.level += n
+        return true
+    }
+    
+    func decreasePlayerLevel(_ n: Int, id: Int) -> Bool  {
+        guard var player = getPlayer(id: id) else { return false }
+        player.level -= n
+        return true
+    }
+    
+    func increaseManchkinBonus(_ n: Int, id: Int) -> Bool  {
+        guard let manchkin = getManchkin(id: id) else { return false }
+        manchkin.bonus += n
+        return true
+    }
+    
+    func decreaseManchkinBonus(_ n: Int, id: Int) -> Bool  {
+        guard let manchkin = getManchkin(id: id) else { return false }
+        manchkin.bonus -= n
+        return true
+    }
+    
+//    func increaseManchkinLevel(_ n: Int, id: Int) -> Bool  {
+//        guard let manchkin = getManchkin(id: id) else { return false }
+//        manchkin.level += n
+//    }
+//
+//    func decreaseManchkinLevel(_ n: Int, id: Int) -> Bool  {
+//        guard let manchkin = getManchkin(id: id) else { return false }
+//        manchkin.level -= n
+//    }
+//
+//    func increaseMonsterLevel(_ n: Int, id: Int) -> Bool  {
+//        guard let monster = getMonster(id: id) else { return false }
+//        monster.level += n
+//    }
+//
+//    func decreaseMonsterLevel(_ n: Int, id: Int) -> Bool  {
+//        guard let monster = getMonster(id: id) else { return false }
+//        monster.level -= n
+//    }
+
+    
+    func changeCourseState(id: Int) -> Bool {
+        guard let manchkin = getManchkin(id: id) else { return false }
+        manchkin.isCursedNow = !manchkin.isCursedNow
+        return true
+    }
+    
+    func getStreight(id: Int) -> Int?  {
+        guard let player = getPlayer(id: id) else { return nil }
+        return player.streight
+    }
+    
+    func isCursed(id: Int) -> Bool {
+        guard let manchkin = getManchkin(id: id) else { return false }
+        return manchkin.isCursedNow
+    }
+    
+    func getStoredManchkins() -> Array<Manchkin> {
+        guard let array = UserDefaults.standard.array(forKey: storedPlayers) as? Array<[String: Any]>, array.count > 0 else {
+            return [Manchkin]()
+        }
+        let manchkins = array.flatMap(Manchkin.init(dict:))
+        return manchkins
+    }
+    
+    func removeStoredManchkin(name: String) {
+        storedManchkins = storedManchkins.filter{ $0.storedName != name }
+    }
+    
+    private func getPlayer(id: Int) -> Player? {
+        if  let manchkin = getManchkin(id: id) {
+            return manchkin
+        }
+        if let monster = getMonster(id: id) {
+            return monster
+        }
+        return nil
+    }
+    
+    private func getStoredManchkin(name: String) -> Manchkin? {
+        let manchkin = storedManchkins.filter{ $0.storedName == name }
+        return manchkin.first
+    }
+    
+    private func createAndStoreIdBaseIfNeed() {
+        guard let id = UserDefaults.standard.object(forKey: playerIdBaseKey) as? Int, id != 0 else {
+            let uuid = UUID().hashValue
+            UserDefaults.standard.set(uuid, forKey: playerIdBaseKey)
+            return
+        }
+        // the base id already exist so nothing to do
     }
     
     private var manchkins: Array<Manchkin> = []
     private var monsters: Array<Monster> = []
-    static private let playerIdKey = "playerId"
     
+    private var playerFabric: PlayerFabric = SimplePlayerFabric()
+    private var storedManchkins: Array<Manchkin>! {
+        didSet {
+            UserDefaults.standard.setValue(storedManchkins, forKey: storedPlayers)
+        }
+    }
 }
